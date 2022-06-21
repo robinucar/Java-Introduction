598a88f5f228df944f9fcb10a861aee06c7f6e73
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-06-21 19:26:57 +0100

    set variables

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 45;
+        int num3 = 30;
+        int result  = (num1 * num2) - num3;
+        System.out.println(result);
+    }
+
+}
